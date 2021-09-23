3419b44a2c9b66f8b8b0ff980b8ea909924e98ea
Abhinav T K <63438668+abhinavtk@users.noreply.github.com>
2021-09-24 01:05:04 +0530

    fix- alert when input values are negative

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ const output = document.querySelector("#output");
 function calculateProfitOrLoss(initial, quantity, current){
     if (initial < current){
         var profit = (current - initial)*quantity ; 
-        var profitPercentage = ((profit/initial)*100).toFixed(2) ;
+        var profitPercentage = (((current - initial)/initial)*100).toFixed(2) ;
          
         showOutput(`Your Profit is ${profit} & profit percentage is ${profitPercentage}%`,'green');
     }
     else if (initial > current){
         var loss = (initial - current)*quantity ; 
-        var lossPercentage = ((loss/initial)*100).toFixed(2) ; 
+        var lossPercentage = (((initial - current)/initial)*100).toFixed(2) ; 
         showOutput(`Your Loss is ${loss} & Loss percentage is ${lossPercentage}%`,'red');
     }
     else{
@@ -42,6 +42,10 @@ function showOutput(msg, color){
 function validateInput(initial, quantity, current) {
     if(initial.length==0 || quantity.length==0 || current.length==0 ){
         alert('Please fill out all the fields.');
+        return false;
+    }
+    if(Number(initial)<=0 || Number(quantity)<=0 || Number(current)<0 ){
+        alert('The values should be positive');
         return false;
     }
     return true;
